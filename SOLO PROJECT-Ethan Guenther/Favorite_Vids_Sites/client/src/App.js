3fe63a231953eb8d04fc8d9160656601cc5a87c0
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-08-06 16:49:45 -0500

    Created display all

---
+++ SOLO PROJECT-Ethan Guenther/Favorite_Vids_Sites/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DisplayAll from './components/displayAll';
+function App() {
+  return (
+    <div className="App">
+      <DisplayAll/>
+    </div>
+  );
+}
+
+export default App;
---
+++ SOLO PROJECT-Ethan Guenther/Favorite_Vids_Sites/client/src/components/displayAll.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayAll = () => {
+  return (
+    <div>
+        <h1>HELLO</h1>
+    </div>
+  )
+}
+
+export default DisplayAll;
